fb5601f52987a41f453f7894642bb5a2c6bc7b2f
Shaun Turney <shaun1192016@gmail.com>
2025-10-16 14:52:23 -0700

    Successful API connection in FinanceAPI.

--- src/main/java/FinanceAPI.java
+++ src/main/java/FinanceAPI.java
@@ -1,9 +1,21 @@
 import com.github.oscerd.finnhub.client.FinnhubClient;
-import com.github.oscerd.finnhub.model.Quote;
+import com.github.oscerd.finnhub.models.CompanyProfile;
+import com.github.oscerd.finnhub.models.CompanyProfile2;
+import com.github.oscerd.finnhub.models.Quote;
+import com.github.oscerd.finnhub.models.Quote;
 
 public class FinanceAPI {
-    private static String API_KEY = "";
-    private static final String BASE_URL = "";
+    public static void main(String[] args) {
+        String API_KEY = "d3n92hpr01qk6516am30d3n92hpr01qk6516am3g";
 
-
+        FinnhubClient client = new FinnhubClient(API_KEY);
+        try {
+            CompanyProfile2 profile = client.companyProfile("AAPL");
+            System.out.println("Name: " + profile.getName());
+            System.out.println("Currency: " + profile.getCurrency());
+            System.out.println("IPO Date: " + profile.getIpo());
+        } catch (Exception e){
+            System.out.println("error");
+    }
+    }
 }
