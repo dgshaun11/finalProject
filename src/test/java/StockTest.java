64901317891f03a3189f7d7892a2833f07128075
Shaun Turney <shaun1192016@gmail.com>
2025-10-21 14:55:34 -0700

    Portfolio prints properly.

--- src/test/java/StockTest.java
+++ src/test/java/StockTest.java
@@ -6,6 +6,7 @@ public class StockTest {
         Stock stock = new Stock("Apple",260, "APPL");
         System.out.println(stock.getStockPrice());
     }
+    @Test
     public void testIfGetTickerWorks(){
         Stock stock = new Stock("Apple", 260, "APPL");
         System.out.println(stock.getTickerName());
