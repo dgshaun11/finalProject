1f480bd017b58cc2d297d9ac0c8bfa7dc984af63
Shaun Turney <shaun1192016@gmail.com>
2025-10-21 14:42:10 -0700

    Add testing for Stock class.

--- src/test/java/StockTest.java
+++ src/test/java/StockTest.java
@@ -1,3 +1,13 @@
-public class StockTest {
+import org.junit.jupiter.api.Test;
 
+public class StockTest {
+    @Test
+    public void testIfGetPriceWorks(){
+        Stock stock = new Stock("Apple",260, "APPL");
+        System.out.println(stock.getStockPrice());
+    }
+    public void testIfGetTickerWorks(){
+        Stock stock = new Stock("Apple", 260, "APPL");
+        System.out.println(stock.getTickerName());
+    }
 }
