716d56677baee763266dd6101f734f2d39ec6401
Shaun Turney <shaun1192016@gmail.com>
2025-10-21 14:54:47 -0700

    Portfolio prints properly.

--- src/main/java/Portfolio.java
+++ src/main/java/Portfolio.java
@@ -3,5 +3,15 @@ import java.util.ArrayList;
 public class Portfolio {
     ArrayList<Stock> portfolio = new ArrayList<>();
 
+    public void addStockToPortfolio(Stock stock){
+        portfolio.add(stock);
+    }
+
+    public void printPortfolio(){
+        System.out.println("------ Portfolio ------");
+        for (Stock stock: portfolio){
+            System.out.println(stock.toString());
+        }
+    }
 
 }
